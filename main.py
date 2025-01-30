18609d5e07ea40c3f581789b76ce62081b569670
Riley Swedberg <swedbergr@unk.edu>
2025-01-30 04:54:29 +0000

    Sections 1.1 through 1.4

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print("Welcome to Cyber 304!")
+# Get elements of matrix A
+A = []
+for row in range(1,3):
+    single_row = []
+    for column in range(1,3):
+        # Create elements in a single row in matrix A
+        single_row.append(int(input(f"A{row}{column}: ")))
+
+    # Add entire row to matrix A
+    A.append(single_row)
+
+# Get elements of vector b
+b = []
+b.append(int(input("b11: ")))
+b.append(int(input("b21: ")))
+
+# TODO
+solution = []
+
+
+# Output solution
+print(solution)
