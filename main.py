5083d8bb84cf41d85e6936318ba049d5cce9125d
Riley Swedberg <swedbergr@unk.edu>
2026-02-11 23:24:41 -0600

    1.6 Matrix Equations Assignment

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
-# Define the function below to verify that two matrices are compatible and multiply them together
-def multiply(matrix1, matrix2):
+# Define the function below to verify that the equation has a solution and solves the equation (if possible)
+# You may copy and paste functions from previous assignments to make calls in this function.
+def solve(matrix1, matrix2):
     '''
-    Matrix takes two 2D lists, verifies their compatibility, multiplies them together
-    and then returns the product matrix as a 2D list.
+    Function takes two 2D square lists, verifies a solution is possible, and computes the solution
+    to the matrix equation that is in the form matrix1 * X = matrix2
     @param matrix1: 2D list of numeric data
     @param matrix2: 2D list of numeric data
-    @return: 2D list of numeric data or alert if not compatible
+    @return: 2D list of numeric data or alert if solution not possible
     '''
     pass    
 
 def get_matrix():
     # Get rows and columns from user
-    rows = int(input("Enter the number of rows: "))
-    columns = int(input("Enter the number of columns: "))
+    rows = int(input("Enter the number of rows and columns: "))
 
     # Create empty list to store matrix values
     matrix = []
@@ -21,7 +21,7 @@ def get_matrix():
     for r in range(rows):
         # Create empty list to store each row
         row_list = []
-        for c in range(columns):
+        for c in range(rows):
             # Get the value for the matrix position and append it to the row
             value = int(input(f"Enter the value for element {r+1}, {c+1}: "))
             row_list.append(value)
@@ -37,17 +37,20 @@ def main():
     Main function for the program.
     '''
     # Get matrix1 values
-    print("Complete information for the first matrix.")
+    print("Complete information for the coefficient matrix.")
     matrix1 = get_matrix()
 
     # Get matrix2 values
-    print("Complete information for the second matrix.")
+    print("Complete information for the solution matrix.")
     matrix2 = get_matrix()
 
-    # Get product
-    product = multiply(matrix1, matrix2)
+    # Display equation
+    print(f"{matrix1} * X = {matrix2}")
 
-    print(product)
+    # Get product
+    solution = solve(matrix1, matrix2)
+
+    print(f"X = {solution}")
 
 
 if __name__ == "__main__":
